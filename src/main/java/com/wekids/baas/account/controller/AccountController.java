4beea2620839c3f81a975472c4a8e73f4bfd7aa2
YE-EUN <musiclove529@naver.com>
2024-11-13 12:00:55 +0900

    [feat/#1]: 컨트롤러 생성

---
+++ src/main/java/com/wekids/baas/account/controller/AccountController.java
@@ -0,0 +1,11 @@
+package com.wekids.baas.account.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+@RequiredArgsConstructor
+public class AccountController {
+}
---
+++ src/main/java/com/wekids/baas/accountTransaction/controller/AccountTransactionController.java
@@ -0,0 +1,11 @@
+package com.wekids.baas.accountTransaction.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+@RequiredArgsConstructor
+public class AccountTransactionController {
+}
