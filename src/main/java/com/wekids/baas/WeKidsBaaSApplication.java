66010d794b20b1953cd510be588a093eb05bc054
YE-EUN <musiclove529@naver.com>
2024-11-13 11:37:41 +0900

    [feat/#1]: 프로젝트 생성

---
+++ src/main/java/com/wekids/baas/WeKidsBaaSApplication.java
@@ -0,0 +1,13 @@
+package com.wekids.baas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeKidsBaaSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeKidsBaaSApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wekids/baas/WeKidsBaaSApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wekids.baas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeKidsBaaSApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
