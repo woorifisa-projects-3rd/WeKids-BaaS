d759e26a44bf1c651074d393f9d566f7b1f006f6
YE-EUN <musiclove529@naver.com>
2024-11-18 10:36:20 +0900

    [feat/#3]: 계좌 생성 컨트롤러 구현

--- src/main/java/com/wekids/baas/account/controller/AccountController.java
+++ src/main/java/com/wekids/baas/account/controller/AccountController.java
@@ -1,11 +1,25 @@
 package com.wekids.baas.account.controller;
 
+import com.wekids.baas.account.dto.request.AccountCreateRequest;
+import com.wekids.baas.account.dto.response.AccountCreateResponse;
+import com.wekids.baas.account.service.AccountService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api/v1/accounts")
 @RequiredArgsConstructor
 public class AccountController {
+    private final AccountService accountService;
+
+    @PostMapping
+    public ResponseEntity<AccountCreateResponse> createAccount(@RequestBody AccountCreateRequest accountCreateRequest) {
+        AccountCreateResponse response = accountService.createAccount(accountCreateRequest);
+
+        return ResponseEntity.ok(response);
+    }
 }
