359821714a7b5ae2729ebac4bf24b7032be1980d
YE-EUN <musiclove529@naver.com>
2024-11-18 10:43:57 +0900

    [feat/#3]: BaseTime SuperBuilder 적용

--- src/main/java/com/wekids/baas/common/entity/BaseTime.java
+++ src/main/java/com/wekids/baas/common/entity/BaseTime.java
@@ -3,7 +3,11 @@ package com.wekids.baas.common.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -12,6 +16,9 @@ import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
+@SuperBuilder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTime {
     @CreatedDate
