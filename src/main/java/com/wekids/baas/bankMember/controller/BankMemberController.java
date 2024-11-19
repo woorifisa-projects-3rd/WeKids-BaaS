bcae4db5bb1938443d8a730df8480c7ebaa56cff
YE-EUN <musiclove529@naver.com>
2024-11-19 13:35:22 +0900

    [feat/#8]: 은행 고객 생성 컨트롤러 구현

--- src/main/java/com/wekids/baas/bankMember/controller/BankMemberController.java
+++ src/main/java/com/wekids/baas/bankMember/controller/BankMemberController.java
@@ -1,11 +1,28 @@
 package com.wekids.baas.bankMember.controller;
 
+import com.wekids.baas.bankMember.dto.request.BankMemberCreateRequest;
+import com.wekids.baas.bankMember.dto.response.BankMemberCreateResponse;
+import com.wekids.baas.bankMember.service.BankMemberService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api/v1/bank-members")
 @RequiredArgsConstructor
 public class BankMemberController {
+    private final BankMemberService bankMemberService;
+
+    @PostMapping
+    public ResponseEntity<BankMemberCreateResponse> createBankMember(@RequestBody @Valid BankMemberCreateRequest bankMemberCreateRequest) {
+        BankMemberCreateResponse response = bankMemberService.createBankMember(bankMemberCreateRequest);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
 }
