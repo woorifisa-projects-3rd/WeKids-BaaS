4ee44931b4f01320a5c07017c11ed8db70ee3ec4
YE-EUN <musiclove529@naver.com>
2024-11-20 11:28:41 +0900

    [feat/#12]: 응답 데이터 Enum 타입으로 받을 수 있는 데이터들 Enum으로 변경

--- src/main/java/com/wekids/baas/account/dto/response/MemberAccountGetResponse.java
+++ src/main/java/com/wekids/baas/account/dto/response/MemberAccountGetResponse.java
@@ -1,9 +1,16 @@
 package com.wekids.baas.account.dto.response;
 
+import com.wekids.baas.account.domain.Account;
+import com.wekids.baas.account.domain.enums.AccountState;
+import com.wekids.baas.bankMember.domain.BankMember;
+import com.wekids.baas.product.domain.Product;
+import com.wekids.baas.product.domain.enums.ProductType;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Builder
@@ -11,8 +18,29 @@ public class MemberAccountGetResponse {
     private String accountNumber;
     private String bankName;
     private BigDecimal balance;
-    private String state;
+    private AccountState state;
     private String bankMemberName;
     private String productName;
-    private String productType;
+    private ProductType productType;
+
+    public static MemberAccountGetResponse from(Account account) {
+        BankMember bankMember = account.getBankMember();
+        Product product = account.getProduct();
+
+        return MemberAccountGetResponse.builder()
+                .accountNumber(account.getAccountNumber())
+                .bankName(account.getBankCode().getName())
+                .balance(account.getBalance())
+                .state(account.getState())
+                .bankMemberName(bankMember.getName())
+                .productName(product.getName())
+                .productType(product.getType())
+                .build();
+    }
+
+    public static List<MemberAccountGetResponse> from(List<Account> accounts) {
+        return accounts.stream()
+                .map(account -> from(account))
+                .collect(Collectors.toList());
+    }
 }
