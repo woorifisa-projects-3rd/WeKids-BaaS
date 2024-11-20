abb77f5145cc0bc975b8e91bab81623c0cefb958
YE-EUN <musiclove529@naver.com>
2024-11-20 19:25:41 +0900

    [feat/#17]: 카드 생성 시 bankMemberName이 아니라 bankMemberId 받도록 수정

--- src/main/java/com/wekids/baas/card/dto/request/CardCreateRequest.java
+++ src/main/java/com/wekids/baas/card/dto/request/CardCreateRequest.java
@@ -1,6 +1,8 @@
 package com.wekids.baas.card.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,9 +13,9 @@ public class CardCreateRequest {
     @NotBlank
     @Size(min = 13, max = 13)
     private String accountNumber;
-    @NotBlank
-    @Size(min = 2)
-    private String bankMemberName;
+    @NotNull
+    @Positive
+    private Long bankMemberId;
     @NotBlank
     @Size(min = 4, max = 4)
     private String password;
