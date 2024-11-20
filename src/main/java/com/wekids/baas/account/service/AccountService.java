a8899a60cfe03f2bddc83081aebdbb05e28143ef
YE-EUN <musiclove529@naver.com>
2024-11-20 11:24:31 +0900

    [feat/#12]: 고객 계좌 조회 서비스 코드 구현

--- src/main/java/com/wekids/baas/account/service/AccountService.java
+++ src/main/java/com/wekids/baas/account/service/AccountService.java
@@ -2,7 +2,11 @@ package com.wekids.baas.account.service;
 
 import com.wekids.baas.account.dto.request.AccountCreateRequest;
 import com.wekids.baas.account.dto.response.AccountCreateResponse;
+import com.wekids.baas.account.dto.response.MemberAccountGetResponse;
+
+import java.util.List;
 
 public interface AccountService {
     AccountCreateResponse createAccount(AccountCreateRequest accountCreateRequest);
+    List<MemberAccountGetResponse> getMemberAccountList(Long baasMemberId, Long bankMemberId);
 }
