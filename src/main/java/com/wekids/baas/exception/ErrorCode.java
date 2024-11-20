ef11a593be83da97ebec75cb1ba74af2fed3d961
YE-EUN <musiclove529@naver.com>
2024-11-20 16:11:15 +0900

    [feat/#13]: 에러 코드 추가

--- src/main/java/com/wekids/baas/exception/ErrorCode.java
+++ src/main/java/com/wekids/baas/exception/ErrorCode.java
@@ -12,7 +12,10 @@ public enum ErrorCode {
     BANK_MEMBER_NOT_FOUND(NOT_FOUND, "존재하지 않는 은행 고객입니다."),
     PRODUCT_NOT_FOUND(NOT_FOUND, "존재하지 않는 계좌 상품입니다."),
     ACCOUNT_NOT_FOUND(NOT_FOUND, "존재하지 않는 계좌입니다."),
-    BAAS_MEMBER_NOT_FOUND(NOT_FOUND, "존재하지 않는 BaaS 고객입니다.");
+    BAAS_MEMBER_NOT_FOUND(NOT_FOUND, "존재하지 않는 BaaS 고객입니다."),
+    ACCOUNT_INACTIVE(BAD_REQUEST, "비활성 계좌입니다."),
+    INSUFFICIENT_BALANCE(BAD_REQUEST, "잔액이 부족합니다."),
+    SENDER_AND_RECEIVER_SAME(BAD_REQUEST, "보내는 분과 받는 분이 같습니다.");
 
     private final HttpStatus status;
     private final String message;
