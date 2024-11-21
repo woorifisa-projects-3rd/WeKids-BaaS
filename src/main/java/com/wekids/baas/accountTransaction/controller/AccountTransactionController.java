b048caab5c288c7d1851ef12e692988079cc4422
YE-EUN <musiclove529@naver.com>
2024-11-21 14:06:37 +0900

    [feat/#14]: 거래 내역 조회 컨트롤러 구현

--- src/main/java/com/wekids/baas/accountTransaction/controller/AccountTransactionController.java
+++ src/main/java/com/wekids/baas/accountTransaction/controller/AccountTransactionController.java
@@ -1,7 +1,5 @@
 package com.wekids.baas.accountTransaction.controller;
 
-import com.wekids.baas.accountTransaction.domain.enums.AccountTransactionType;
-import com.wekids.baas.accountTransaction.domain.enums.CurrencyCode;
 import com.wekids.baas.accountTransaction.dto.request.TransactionGetRequest;
 import com.wekids.baas.accountTransaction.dto.request.TransferRequest;
 import com.wekids.baas.accountTransaction.dto.response.TransactionGetResponse;
@@ -12,7 +10,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -30,17 +27,7 @@ public class AccountTransactionController {
 
     @PostMapping("/getTransactions")
     public ResponseEntity<List<TransactionGetResponse>> getTransactionList(@RequestBody @Valid TransactionGetRequest transactionGetRequest) {
-        TransactionGetResponse transactionGetResponse = TransactionGetResponse.builder()
-                .title("카카오페이")
-                .type(AccountTransactionType.DEPOSIT.name())
-                .amount(1000L)
-                .balance(19862L)
-                .sender("조예은")
-                .receiver("강현우")
-                .currencyCode(CurrencyCode.KRW.name())
-                .transactionDate(LocalDateTime.now())
-                .build();
-        List<TransactionGetResponse> result = List.of(transactionGetResponse);
+        List<TransactionGetResponse> result = accountTransactionService.getTransactionList(transactionGetRequest);
         return ResponseEntity.ok(result);
     }
 
