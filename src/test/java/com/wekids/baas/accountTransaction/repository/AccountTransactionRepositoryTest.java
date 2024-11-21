c149bf08fc51d8ee20770986732c2d8bf128b1f0
YE-EUN <musiclove529@naver.com>
2024-11-21 14:08:14 +0900

    [test/#14]: 거래 내역 조회 테스트

--- src/test/java/com/wekids/baas/accountTransaction/repository/AccountTransactionRepositoryTest.java
+++ src/test/java/com/wekids/baas/accountTransaction/repository/AccountTransactionRepositoryTest.java
@@ -1,8 +1,15 @@
 package com.wekids.baas.accountTransaction.repository;
 
+import com.wekids.baas.accountTransaction.domain.AccountTransaction;
+import com.wekids.baas.accountTransaction.domain.enums.AccountTransactionType;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+
+import java.time.*;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,7 +19,16 @@ class AccountTransactionRepositoryTest {
     AccountTransactionRepository accountTransactionRepository;
 
     @Test
-    void test() {
-        accountTransactionRepository.findAll();
+    void findAccountTransactionByCondition() {
+        LocalDateTime start = LocalDateTime.of(2024, 11, 20, 0, 0, 0);
+        LocalDateTime end = LocalDateTime.of(2024, 11, 25, 23, 59, 59);
+
+        PageRequest pageRequest = PageRequest.of(1, 3);
+
+        List<AccountTransaction> transactions = accountTransactionRepository.findAccountTransactionsByCondition("1002913023908", start, end, AccountTransactionType.WITHDRAW, pageRequest);
+
+        System.out.println("transactions = " + transactions);
+
+        Assertions.assertThat(transactions.size()).isEqualTo(3);
     }
 }
