4316bb2698a3bf8ceb828075c48d57cc9de8f1f7
YE-EUN <musiclove529@naver.com>
2024-11-21 21:00:50 +0900

    [feat/#14]: 요청을 위한 type enum 생성

---
+++ src/main/java/com/wekids/baas/accountTransaction/dto/request/AccountTransactionRequestType.java
@@ -0,0 +1,25 @@
+package com.wekids.baas.accountTransaction.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.wekids.baas.exception.BaasException;
+import com.wekids.baas.exception.ErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.stream.Stream;
+
+@Getter
+@AllArgsConstructor
+public enum AccountTransactionRequestType {
+    ALL,
+    DEPOSIT,
+    WITHDRAW;
+
+    @JsonCreator
+    public static AccountTransactionRequestType parsing(String value) {
+        return Stream.of(AccountTransactionRequestType.values())
+                .filter(type -> type.toString().equals(value.toUpperCase()))
+                .findFirst()
+                .orElseThrow(() -> new BaasException(ErrorCode.INVALID_INPUT, "거래 타입: "  + value));
+    }
+}
