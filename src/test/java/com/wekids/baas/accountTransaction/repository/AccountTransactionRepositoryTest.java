8cb8bd5354481edcb3b9e2bb91098671cacef07c
YE-EUN <musiclove529@naver.com>
2024-11-21 21:02:02 +0900

    [feat/#14]: 거래 내역 조회 서비스 코드 테스트

--- src/test/java/com/wekids/baas/accountTransaction/repository/AccountTransactionRepositoryTest.java
+++ src/test/java/com/wekids/baas/accountTransaction/repository/AccountTransactionRepositoryTest.java
@@ -4,6 +4,8 @@ import com.wekids.baas.accountTransaction.domain.AccountTransaction;
 import com.wekids.baas.accountTransaction.domain.enums.AccountTransactionType;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.PageRequest;
@@ -18,17 +20,35 @@ class AccountTransactionRepositoryTest {
     @Autowired
     AccountTransactionRepository accountTransactionRepository;
 
-    @Test
-    void findAccountTransactionByCondition() {
-        LocalDateTime start = LocalDateTime.of(2024, 11, 20, 0, 0, 0);
-        LocalDateTime end = LocalDateTime.of(2024, 11, 25, 23, 59, 59);
+    @ParameterizedTest
+    @CsvSource({"ALL, 4", "DEPOSIT, 0", "WITHDRAW, 1"})
+    void findAccountTransactionByCondition(String type, int answer) {
+        LocalDateTime start = LocalDateTime.of(LocalDate.of(2024, 11, 1), LocalTime.MIN);
+        LocalDateTime end = LocalDateTime.of(LocalDate.of(2024, 11, 30), LocalTime.MAX);
+        AccountTransactionType txType = type.equals("ALL") ? null : AccountTransactionType.valueOf(type);
 
-        PageRequest pageRequest = PageRequest.of(1, 3);
+        PageRequest pageRequest = PageRequest.of(1, 5);
 
-        List<AccountTransaction> transactions = accountTransactionRepository.findAccountTransactionsByCondition("1002913023908", start, end, AccountTransactionType.WITHDRAW, pageRequest);
+        List<AccountTransaction> transactions = accountTransactionRepository.findAccountTransactionsByCondition("1002913023908", start, end, txType, pageRequest);
 
         System.out.println("transactions = " + transactions);
 
-        Assertions.assertThat(transactions.size()).isEqualTo(3);
+        Assertions.assertThat(transactions.size()).isEqualTo(answer);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"2024, 11, 20, 2024, 11, 21, 3", "2024, 11, 20, 2024, 11, 24, 3"})
+    void findAccountTransactionByCondition_date_condition(int startYear, int startMonth, int startDay, int endYear, int endMonth, int endDay, int answer) {
+        LocalDateTime start = LocalDateTime.of(LocalDate.of(startYear, startMonth, startDay), LocalTime.MIN);
+        LocalDateTime end = LocalDateTime.of(LocalDate.of(endYear, endMonth, endDay), LocalTime.MAX);
+        AccountTransactionType txType = null;
+
+        PageRequest pageRequest = PageRequest.of(0, 5);
+
+        List<AccountTransaction> transactions = accountTransactionRepository.findAccountTransactionsByCondition("1002913023908", start, end, txType, pageRequest);
+
+        System.out.println("transactions = " + transactions);
+
+        Assertions.assertThat(transactions.size()).isEqualTo(answer);
     }
 }
