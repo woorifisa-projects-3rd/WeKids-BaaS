fa56da4c56172de7e6fba2d5e6317f73924c42d0
YE-EUN <musiclove529@naver.com>
2024-11-26 12:44:20 +0900

    [feat/#24]: bankMemberId 조회 컨트롤러 추가

--- src/main/java/com/wekids/baas/bankMember/controller/BankMemberController.java
+++ src/main/java/com/wekids/baas/bankMember/controller/BankMemberController.java
@@ -1,16 +1,14 @@
 package com.wekids.baas.bankMember.controller;
 
 import com.wekids.baas.bankMember.dto.request.BankMemberCreateRequest;
-import com.wekids.baas.bankMember.dto.response.BankMemberCreateResponse;
+import com.wekids.baas.bankMember.dto.request.BankMemberIdGetRequest;
+import com.wekids.baas.bankMember.dto.response.BankMemberIdResponse;
 import com.wekids.baas.bankMember.service.BankMemberService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/bank-members")
@@ -19,10 +17,15 @@ public class BankMemberController {
     private final BankMemberService bankMemberService;
 
     @PostMapping
-    public ResponseEntity<BankMemberCreateResponse> createBankMember(@RequestBody @Valid BankMemberCreateRequest bankMemberCreateRequest) {
-        BankMemberCreateResponse response = bankMemberService.createBankMember(bankMemberCreateRequest);
+    public ResponseEntity<BankMemberIdResponse> createBankMember(@RequestBody @Valid BankMemberCreateRequest bankMemberCreateRequest) {
+        BankMemberIdResponse response = bankMemberService.createBankMember(bankMemberCreateRequest);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
+    @PostMapping("/getId")
+    public ResponseEntity<BankMemberIdResponse> getBankMemberId(@RequestBody @Valid BankMemberIdGetRequest bankMemberIdGetRequest) {
+        BankMemberIdResponse bankMemberId = bankMemberService.getBankMemberId(bankMemberIdGetRequest);
+        return ResponseEntity.ok(bankMemberId);
+    }
 }
